b10a5e78175b774cfb555f0a43bdfed0dcac6b19
freexploit <infotux.valerio@gmail.com>
2013-07-19 19:19:00 -0600

    modified:   nbproject/project.properties 	new file:   pdfbox-app-1.8.2.jar 	new file:   src/negocio/cargador/PdfCreator.java

---
+++ src/negocio/cargador/PdfCreator.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package negocio.cargador;
+
+import contenedor.Lista;
+import org.apache.pdfbox.pdmodel.PDDocument;
+
+/**
+ *
+ * @author christopher
+ */
+public class PdfCreator {
+    
+    PDDocument pdf;
+    Lista list;
+    
+}
